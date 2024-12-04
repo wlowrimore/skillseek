5f3eb3f9a37f7c52d7c89b5f5a4c5bbee171c785
William Lowrimore <wlowrimore@gmail.com>
2024-12-03 23:36:35 -0600

    adds window size conditional rendering of navbars

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,8 +7,12 @@ export default function Layout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <main className="font-work-sans">
-      <Navbar />
-      <MobileNavbar />
+      <div className="hidden md:block">
+        <Navbar />
+      </div>
+      <div className="block md:hidden">
+        <MobileNavbar />
+      </div>
       {children}
     </main>
   );
