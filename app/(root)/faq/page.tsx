d996e4af9f636aee0b4371ba8c2c82919f9e32b9
William Lowrimore <wlowrimore@gmail.com>
2024-12-18 22:04:17 -0600

    adjusts margin and height for mobile faq content

--- app/(root)/faq/page.tsx
+++ app/(root)/faq/page.tsx
@@ -6,11 +6,11 @@ import React from "react";
 const FAQ = () => {
   return (
     <>
-      <section className="container px-4 w-full md:mx-auto bg-faq  mt-12 flex flex-col justify-center items-center md:mt-16">
+      <section className="container px-4 w-full mx-auto bg-faq mt-12 flex flex-col justify-center items-center md:mt-16">
         <h1 className="w-full font-bold text-3xl mt-8 md:mt-4 md:pt-4 lg:pt-10 pb-4 border-b">
           SkillSeek FAQ
         </h1>
-        <div className="flex justify-center items-start mx-auto pb-10 md:pb-0">
+        <div className="flex justify-center items-start mx-auto pb-0">
           <div className="flex flex-col items-center gap-6 w-full mx-auto md:place-self-center md:pr-4 lg:pl-16 xl:pl-60 md:grid grid-cols-2 md:gap-[6rem] lg:gap-32 md:my-8 lg:my-12">
             <FAQContent />
             <FAQImage />
