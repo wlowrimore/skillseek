cde94e34bc1107d8418bfdf38b51b67a6044abf1
William Lowrimore <wlowrimore@gmail.com>
2025-01-16 10:48:19 -0600

    corrects splash pages placement of elements pass 1

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -38,14 +38,14 @@ const SplashScreen = () => {
   if (!isVisible || isAuthenticated) return null;
 
   return (
-    <div className="fixed inset-0 bg-[url('/splash-screen-bg.webp')] flex flex-col items-center justify-center md:hidden">
-      <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center flex-center bg-black/80 border-[3px] border-zinc-400 rounded-2xl">
-        <div className="relative top-[30%] left-0 flex items-center justify-center">
+    <div className="fixed h-screen left-0 right-0 top-0 bottom-0 bg-[url('/splash-screen-bg.webp')] flex flex-col items-center justify-center md:hidden">
+      <div className="h-screen flex flex-col items-center flex-center border-[3px] border-zinc-400 rounded-2xl">
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
           {/* Shapes */}
           <SplashScreenColorCircles />
           {/* Title */}
           <div className="relative z-50 w-full flex flex-col justify-center items-center">
-            <div className="absolute -top-[15rem] z-60 bg-black/70 w-screen h-screen flex flex-col items-center pt-[17rem] px-4 text-zinc-400">
+            <div className=" bg-black/80 w-screen h-screen flex flex-col items-center pt-[17rem] px-4 text-zinc-400">
               <h1 className="text-6xl text-center font-bold w-full bg-swirl-pattern bg-clip-text text-transparent opacity-60">
                 SkillSeek
               </h1>
@@ -54,27 +54,27 @@ const SplashScreen = () => {
               </p>
             </div>
             {/* Loading Spinner */}
-            <div className="absolute h-[80%] top-[8rem] z-70">
-              <Loader2 className="w-24 h-24 bg-gradient-to-br from-[#51819C] to-[#F29072] text-[#51819C]/80 rounded-full animate-spin p-4" />
-              <div className="relative z-80 bottom-24 w-full">
+            <div className="absolute top-[22%] w-44 h-44 z-70">
+              <Loader2 className="relative top-[100%] w-44 h-44 bg-gradient-to-br from-[#51819C] to-[#F29072] text-[#51819C]/80 rounded-full animate-spin p-6" />
+              <div className="relative z-80  w-full">
                 <Image
                   src="/brand-logo-new.png"
                   alt="skillseekapp"
                   width={500}
                   height={500}
-                  className="w-64 h-auto"
+                  className="w-44 h-auto border-2 border-slate-800 rounded-full"
                   onClick={handleAuthentication}
                 />
               </div>
             </div>
             <SignInVerificationWrapper>
               {!session?.user ? (
-                <div className="absolute z-90 w-screen flex justify-center items-end top-[24vh] -left-[13.4rem]  text-zinc-300 text-[1.3rem]">
-                  <span className="bg-slate-800 relative min-w-[100vw] left-[13%] h-[0.2rem] rounded-full"></span>
+                <div className="absolute z-90 flex justify-center items-end text-zinc-300 text-[1.3rem]">
+                  {/* <span className="bg-slate-800 relative min-w-[100vw] left-[13%] h-[0.2rem] rounded-full"></span> */}
                   <form onSubmit={handleSubmit}>
                     <button
                       type="submit"
-                      className="relative flex justify-center right-[49.95vw] items-center top-[1.7rem] z-20 py-2 px-4"
+                      // className="relative flex justify-center right-[49.95vw] items-center top-[1.7rem] z-20 py-2 px-4"
                     >
                       <p className="w-fit px-3 py-0.5 border-[3px] bg-gray-900 border-slate-800 rounded-md flex justify-center items-center">
                         <span>Enter</span>
--- components/ui/SplashScreenColorCircles.tsx
+++ components/ui/SplashScreenColorCircles.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const SplashScreenColorCircles = () => {
   return (
-    <>
+    <div className="h-screen">
       <div className="absolute right-[3.5rem] top-[-13rem] w-20 h-20 bg-slate-400 border-[3px] border-zinc-400 rounded-full"></div>
       <div className="absolute left-[9rem] top-[-18rem] w-72 h-72 bg-[#B0CBC7] border-[3px] border-zinc-400 rounded-full"></div>
       <div className="absolute left-[7rem] top-[12rem] w-32 h-32 bg-[#275975] border-[3px] border-zinc-400 rounded-full"></div>
       <div className="absolute right-[6rem] bg-[#F29072] border-[3px] w-64 h-64 border-zinc-400 rounded-2xl rotate-45"></div>
       <div className="absolute right-[7rem] top-[20rem] w-96 h-96 bg-[#F1C7A3] border-[3px] border-zinc-400 rounded-full"></div>
       <div className="absolute left-[2rem] top-[34rem] w-44 h-64 bg-[#51819C] border-[3px] border-zinc-400 rounded-4xl -rotate-[45deg]"></div>
-    </>
+    </div>
   );
 };
 
